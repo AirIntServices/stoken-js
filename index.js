ffafc528884d9e947263323a6f68cb899d593566
dlajarretie <dlajarretie@airintservices.com>
2020-08-12 18:35:46 +0200

    fixed infinite wait when calling the lib after initialisation

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const securid = require("./lib/securid");
 
-const stoken_compute_code = securid.cwrap("stoken_compute_code", "string", [
-  "string",
-  "string"
-]);
+const api = { initialized : false};
 
+const wrapFunction = () => {
+  api.stoken_compute_code = securid.cwrap("stoken_compute_code", "string", [
+    "string",
+    "string"
+  ]);
+  api.initialized = true;
+}
 
-exports.computeCode = (token, pin) => {
-  
-  return new Promise((resolve, reject) => {
-    
-    securid["onRuntimeInitialized"] = _ => {
-      
-      const code = stoken_compute_code(token, pin);
+securid["onRuntimeInitialized"] = wrapFunction;
+
+const generateCode = (token, pin) => {
+  const code = api.stoken_compute_code(token, pin);
 
       switch (code) {
         case "ERR_TOKEN_GARBLED":
-          reject(new Error("Token string provided is garbled"));
-          break;
+          throw new Error("Token string provided is garbled");
         case "ERR_TOKEN_FAILED_SIZEOF":
-          reject(new Error("Failed to allocate memory for token."));
-          break;
+          throw new Error("Failed to allocate memory for token.");
         case "ERR_TOKEN_DECRYPT_FAILED":
-          reject(new Error("error while decrypting token."));
-          break;
+          throw new Error("error while decrypting token.");
 
         default:
-          resolve(code);
-          break;
+          return code;
       }
-    };
+}
 
+exports.computeCode = (token, pin) => {
+
+  if(api.initialized) {
+    return Promise.resolve(generateCode(token, pin));
+  }
+
+  return new Promise((resolve, reject) => {
+    securid["onRuntimeInitialized"] = _ => {
+      wrapFunction();
+      try {
+        const code = generateCode(token, pin);
+        resolve(code);
+      } catch(err) {
+        reject(err);
+      }
+    }
   });
 
 };
